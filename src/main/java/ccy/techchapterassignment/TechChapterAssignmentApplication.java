3212fd3b3bff4988fd18eb24ff0bc240383f4037
Chris <chye0001@stud.kea.dk>
2025-03-13 17:49:58 +0100

    added dependencies

---
+++ src/main/java/ccy/techchapterassignment/TechChapterAssignmentApplication.java
@@ -0,0 +1,13 @@
+package ccy.techchapterassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChapterAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChapterAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ccy/techchapterassignment/TechChapterAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ccy.techchapterassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChapterAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
